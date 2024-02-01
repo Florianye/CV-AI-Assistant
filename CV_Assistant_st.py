7550ac4f0bbc3dbf3f9eafe5fe600bd3c39f8048
Florianye <150835814+Florianye@users.noreply.github.com>
2024-02-01 21:18:56 +0000

    add streamlit

--- CV_Assistant_st.py
+++ CV_Assistant_st.py
@@ -73,31 +73,47 @@ def interact_with_bot(qa_with_source, user_input):
 
 # Main execution block for Streamlit app
 def main():
-
     st.title("CV Chatbot")
     st.write("Ask questions about the CV and get answers from the chatbot.")
 
     qa_with_source = initialize_bot()
 
-    user_input = st.text_input("Ask a question about the CV:", "")
-    with st.chat_message("user"):
-        st.write(user_input)
+    # Store LLM generated responses
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
+    # Display or clear chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
+    def clear_chat_history():
+        st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
+    # User-provided prompt
+    prompt = st.chat_input()
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
 
-    # if user_input:
-    #     response = interact_with_bot(qa_with_source, user_input)
-    #     st.text_area("Response:", response, height=200)
+    #st.write(st.session_state.messages)
 
-
-
-
-    # prompt = st.chat_input("Say something")
-    # if prompt:
-    #     st.write(f"User has sent the following prompt: {prompt}")
-
-        
+    # Generate a new response if last message is not from assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = interact_with_bot(qa_with_source, prompt)
+                st.write(response)
+    #             placeholder = st.empty()
+    #             full_response = ''
+    #             for item in response:
+    #                 full_response += item
+    #                 placeholder.markdown(full_response)
+    #             placeholder.markdown(full_response)
+        message = {"role": "assistant", "content": response}
+        st.session_state.messages.append(message)        
 
 if __name__ == "__main__":
     main()
