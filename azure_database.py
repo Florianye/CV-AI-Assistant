d34f2108b7b5f257529e4069c5fabca850270ca7
Florianye <150835814+Florianye@users.noreply.github.com>
2024-02-08 19:35:40 +0000

    update

--- azure_database.py
+++ azure_database.py
@@ -1,9 +1,10 @@
-import pyodbc as odbc
+import pyodbc
 import os
 import dotenv
 import streamlit as st
 
 #@st.cache_resource(hash_funcs={odbc.connect: id})
+@st.cache_resource
 def conn_database(server_name, database, db_username, db_password, streamlit=False):
     if streamlit == False:
         dotenv.load_dotenv(".env", override=True)
@@ -14,6 +15,6 @@ def conn_database(server_name, database, db_username, db_password, streamlit=Fal
         os.environ["DB_PASSWORD"] = db_password
 
     connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:%s,1433;Database=%s;Uid=%s;Pwd={%s};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;' % (os.environ["SERVER_NAME"], os.environ["DATABASE"], os.environ["DB_USERNAME"], os.environ["DB_PASSWORD"])
-    conn = odbc.connect(connection_string)
+    conn = pyodbc.connect(connection_string)
     
     return conn
