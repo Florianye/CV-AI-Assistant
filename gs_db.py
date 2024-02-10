e24202759ed9ea73ef6fa33d05a5ba4ea8982da3
Florianye <150835814+Florianye@users.noreply.github.com>
2024-02-10 15:56:00 +0000

    update

--- gs_db.py
+++ gs_db.py
@@ -2,6 +2,17 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
+@st.cache_resource(show_spinner=False)
 def init_gs_conn():
     gs_conn = st.connection("gsheets", type=GSheetsConnection)
     return gs_conn
+
+def update_gs(row):
+    chat_history_row = pd.DataFrame([row])
+    if chat_history_row.iloc[0].count() > 2:
+        gs_conn = init_gs_conn()
+        data = gs_conn.read(worksheet="chat-history", usecols=[0, 1, 2], show_spinner=False, ttl=0).dropna()
+        data = pd.concat([data, chat_history_row])
+        gs_conn.update(worksheet="chat-history", data=data)
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 # Main execution block for Streamlit app
 import streamlit as st
 from ai_assistant import initialize_bot, interact_with_bot
-from gs_db import init_gs_conn
+from gs_db import init_gs_conn, update_gs
 from datetime import datetime
 import pandas as pd
 import logging
 
 st.title("Florian Ye's AI Assistant")
-#qa_with_source = initialize_bot()
 
 # Store LLM generated responses
 start_message = "Hi, I'm happy to answer any questions you may have about Florian's professional and educational background!"
@@ -36,8 +35,8 @@ if prompt:
 
         # Save date, questions and answers to defined empty list
         datetime_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        chat_history_row["datetime"] = datetime_now
-        chat_history_row["question"] = prompt.replace("'", "")
+        chat_history_row["datetime"] = datetime_now # Save in dict
+        chat_history_row["question"] = prompt.replace("'", "") # Save in dict
 
 #st.write(st.session_state.messages)
 
@@ -46,22 +45,18 @@ if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = interact_with_bot(prompt)
+            chat_history_row["answer"] = response.replace("'", "") # Save in dict
+            # Save chat_history_row as row in gs
+            try:
+                update_gs(chat_history_row)
+            except:
+                pass
             st.write(response)
-            chat_history_row["answer"] = response.replace("'", "")
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
 
-    # Save chat_history_row as row in gs
-    try:
-        logging.getLogger().setLevel(logging.ERROR) # Hide Running function message
-        chat_history_row = pd.DataFrame([chat_history_row])
-        if chat_history_row.iloc[0].count() > 2:
-            gs_conn = init_gs_conn()
-            data = gs_conn.read(worksheet="chat-history", usecols=[0, 1, 2], ttl=0).dropna()
-            data = pd.concat([data, chat_history_row])
-            gs_conn.update(worksheet="chat-history", data=data)
-    except:
-        pass
+
+    
 
 
 
